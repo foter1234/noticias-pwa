4dae6068b6ab44e270e371ba3d713eba171d88b7
foter1234 <lucaspesteves2017@gmail.com>
2023-10-23 15:46:57 -0400

    newcommit

--- js/main.js
+++ js/main.js
@@ -8,14 +8,22 @@ if ('serviceWorker' in navigator) {
       console.log('Service worker registrada! 😎', reg);
 
       postNews();
+    
     } catch (err) {
       console.log('😥 Service worker registro falhou: ', err);
     }
   });
 }
 
-//064e991ffc094369adf523ed024ef145
 
+  document.getElementById('botao').addEventListener('click', () => {
+      const pesquisar = document.getElementById('pesquisar').value;
+          buscar(pesquisar);
+     
+  });
+  
+
+//064e991ffc094369adf523ed024ef145
 const apiKey = "e531a884462e463aa1ccd0b3178ddcef"
 let url = `https://newsapi.org/v2/top-headlines?country=us&apikey=${apiKey}`;
 const main = document.querySelector('main')
@@ -28,7 +36,6 @@ async function postNews() {
      main.innerHTML = data.articles.map(createArticle).join("\n")
 
      function createArticle(article){
-      console.log(article)
       return `
       <div class="article">
       
@@ -41,3 +48,41 @@ async function postNews() {
       `
      }
 }
+
+async function buscar(pesquisar) {
+
+  let url = `https://newsapi.org/v2/everything?q=${pesquisar}&apikey=${apiKey}`;
+
+  const res =  await fetch(url)
+  const data = await res.json()
+
+  console.log(data.articles);
+  main.innerHTML = data.articles.map(nova).join("\n")
+           
+  function nova(article) {
+  //  if (pesquisar === 0) {
+  //  return `
+  //  <div>
+  //   <p>não tem nada aqui</p>
+//   </div>
+//    `
+    
+
+   /// }else{
+      
+      return `
+      <div class="article">
+      
+      <a href="${article.url}" target="_blank">
+        <img src="${article.urlToImage}" class="image" alt="${article.content}"/>
+        <h2>${article.title}</h2>
+        <p>${article.description}</p>
+        </a>
+      </div>
+      `
+ //   }
+   
+}
+}
+
+
